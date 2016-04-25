f24c2d3ae4c2774894b5ffd1c959be81b87d44d9
RexCorpDev <rexcorpfx@gmail.com>
2016-04-25 11:24:08 -0700

    completed table

--- models/grade.js
+++ models/grade.js
@@ -20,32 +20,33 @@ exports.get = function(cb){
 };
 
 exports.create = function(grade, cb){
-  if(!todo.dueDate || !todo.desc){
+  if(!grade.name || !grade.score || !grade.total){
     return cb('Missing required fields.');
   }
-
   var percent = (grade.score/grade.total)*100;
-  var A = 100 > perecent && percent > 89;
-  var B = 90 > percent && percent > 79;
-  var C = 80 > percent && percent > 69;
-  var D = 70 > percent && percent > 59;
-  var F = 60 > percent;
-  var finalGrade;
+  var finalGrade = '';
 
-  switch(percent) {
-    case A : finalGrade = 'A'; break;
-    case B : finalGrade = 'B'; break;
-    case C : finalGrade = 'C'; break;
-    case D : finalGrade = 'D'; break;
-    case F : finalGrade = 'F'; break;
-    default; finalGrade = 'Did not submit'
+  console.log(percent);
+
+  if(percent <= 100 && percent >= 90) {
+    finalGrade = 'A';
+  } else if (percent <= 89 && percent >= 80) {
+    finalGrade = 'B';
+  } else if(percent <= 79 && percent >= 70) {
+    finalGrade = 'C';
+  } else if (percent <= 69 && percent >= 60) {
+    finalGrade = 'D';
+  } else if(percent <= 59 && percent >= 50) {
+    finalGrade = 'F';
   }
 
+  console.log(finalGrade);
+
   db.run(`INSERT INTO grades (name, score, total, grade) VALUES ( ?, ?, ?, ?)`,
-    grade.name,
-    grade.score,
-    grade.total,
-    finalGrade,
-    cb
-  );
+  grade.name,
+  grade.score,
+  grade.total,
+  finalGrade,
+  cb
+);
 };
--- routes/grades.js
+++ routes/grades.js
@@ -3,8 +3,45 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('all the grades');
+var Grade = require('../models/grade');
+
+
+router.route('/')
+.get((req, res) => {
+  Grade.get((err, grades) => {
+    if(err){
+      return res.status(400).send(err);
+    }
+    res.send(grades);
+  });
 })
+.post((req, res) => {
+  Grade.create(req.body, err => {
+    if(err){
+      return res.status(400).send(err);
+    }
+    res.send();
+  });
+});
 
 module.exports = router;
+//
+// //////
+//
+// router.route('/')
+// .get((req, res) => {
+//   Todo.get((err, todos) => {
+//     if(err){
+//       return res.status(400).send(err);
+//     }
+//     res.send(todos);
+//   });
+// })
+// .post((req, res) => {
+//   Todo.create(req.body, err => {
+//     if(err) {
+//       return res.status(400).send(err);
+//     }
+//     res.send();
+//   });
+// });
